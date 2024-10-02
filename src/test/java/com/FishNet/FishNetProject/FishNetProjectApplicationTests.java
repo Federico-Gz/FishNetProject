2db1e6610e74835cc667f6526d522386d284058a
Federico-Gz <federicograziani96@gmail.com>
2024-10-02 10:09:16 +0200

    creata struttura cartelle

---
+++ src/test/java/com/FishNet/FishNetProject/FishNetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FishNet.FishNetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishNetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
